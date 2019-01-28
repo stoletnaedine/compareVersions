44696d4686cc9d3f7a5d691d4157f97b3b945bcc
Artur Islamgulov <artur.islamgulov@gmail.com>
2019-01-28 17:55:12 +0300

    Use Split()
    Переписал алгоритм вычленения номера версии через метод split().

--- Main.java
+++ Main.java
@@ -1,58 +1,46 @@
 public class Main {
 
 	public static void main(String[] args) {
+		show("Version 1: " + VERS_ONE);
+		show("Version 2: " + VERS_TWO);
 		show("Ok, let's define the old version!");
-		show("Version 1: " + VERSION_ONE);
-		show("Version 2: " + VERSION_TWO);
 		compareVersion(VERS_ONE, VERS_TWO);
 		}
 
-	private static String VERSION_ONE = "8.1.13.41";
-	private static String VERSION_TWO = "8.1.009.125";
-	private static String VERS_ONE = VERSION_ONE + ".";
-	private static String VERS_TWO = VERSION_TWO + ".";
+	private static String VERS_ONE = "8.1.13.41";
+	private static String VERS_TWO = "8.1.009.125";
 
 	public static void compareVersion(String VERS_ONE, String VERS_TWO) {
-		int v1 = strToInt(firstNum(VERS_ONE));
-		int v2 = strToInt(firstNum(VERS_TWO));
-		if (v1 == v2) {
-			show(v1 + " = " + v2 + " and check next num ↓");
-			compareVersion(otherNum(VERS_ONE), otherNum(VERS_TWO));
+		int[] v1 = strToInt(split(VERS_ONE));
+		int[] v2 = strToInt(split(VERS_TWO));
+		for (int i = 0; i < 4; i++) {
+			if (v1[i] == v2[i]) {
+				show(v1[i] + " = " + v2[i] + " check next num ↓");
+			}
+			else if (v1[i] < v2[i]) {
+				show(v1[i] + " < " + v2[i]);
+				show("Version N1 (" + VERS_ONE + ") is old.");
+				break;
+			}
+			else if (v1[i] > v2[i]) {
+				show(v1[i] + " > " + v2[i]);
+				show("Version N2 (" + VERS_TWO + ") is old.");
+				break;
+			}
 		}
-		else if (v1 < v2) {
-			show(v1 + " < " + v2);
-			show("Version N1 (" + VERSION_ONE + ") is old.");
+	}
+
+	private static String[] split(String version) {
+		String[] splitVers = version.split("\\.");
+		return splitVers;
+	}
+
+	private static int[] strToInt(String[] strArr) {
+		int[] intArr = new int[strArr.length];
+		for (int i = 0; i < strArr.length; i++) {
+			intArr[i] = Integer.parseInt(strArr[i]);
 		}
-		else if (v1 > v2) {
-			show(v1 + " > " + v2);
-			show("Version N2 (" + VERSION_TWO + ") is old.");
-		}
-	}
-
-	private static int findDot(String version) {
-		int indexDot = version.indexOf(".");
-		return indexDot;
-	}
-
-	private static String firstNum(String v) {
-		int start = 0;
-		int end = findDot(v);
-		char[] prev = new char[end - start];
-		v.getChars(start, end, prev, 0);
-		return new String(prev);
-	}
-
-	private static String otherNum(String v){
-		int start = findDot(v) + 1;
-		int end = v.length();
-		char[] next = new char[end - start];
-		v.getChars(start, end, next, 0);
-		return new String(next);
-	}
-
-	private static int strToInt(String str) {
-		int x = Integer.parseInt(str);
-		return x;
+		return intArr;
 	}
 
 	private static void show(String result) {
